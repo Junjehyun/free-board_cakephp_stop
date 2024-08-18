e9723cfde112cbf71ada9b8592e180acbf3af9a8
ジェヒョン <h94051987@gmail.com>
2024-08-18 17:19:12 +0900

    레이아웃 색상 변경

--- templates/element/footer.php
+++ templates/element/footer.php
@@ -1,5 +1,5 @@
 <!-- src/Template/Element/footer.php -->
-<footer class="bg-pink-50 text-gray-700 py-4 text-center mt-10">
+<footer class="bg-slate-50 text-gray-700 py-4 text-center mt-10">
     <p>&copy; <?= date('Y') ?> UnKnown. All rights reserved.</p>
     <p>
         <a href="<?= $this->Url->build('#') ?>" class="text-gray-700 hover:underline">Privacy Policy</a> |
