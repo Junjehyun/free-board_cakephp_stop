35746b92b788658c30df402210da3abb37d42f8b
ジェヒョン <h94051987@gmail.com>
2024-09-14 15:25:59 +0900

    240914

--- templates/Main/index.php
+++ templates/Main/index.php
@@ -3,8 +3,8 @@
         <div class="accordian">
             <!-- 80년대 -->
             <div class="accordion-item">
-                <button class="accordion-button">1980's</button>
-                <div class="accordion-content">
+                <button class="accordion-button">1980's ▼</button>
+                <div class="accordion-content space-x-5">
                     <a href="#">1982</a>
                     <a href="#">1983</a>
                     <a href="#">1984</a>
@@ -17,8 +17,8 @@
             </div>
             <!-- 90년대 -->
             <div class="accordion-item">
-                <button class="accordion-button">1990's</button>
-                <div class="accordion-content">
+                <button class="accordion-button">1990's ▼</button>
+                <div class="accordion-content space-x-5">
                     <a href="#">1990</a>
                     <a href="#">1991</a>
                     <a href="#">1992</a>
@@ -33,8 +33,8 @@
             </div>
             <!-- 2000년대 -->
             <div class="accordion-item">
-                <button class="accordion-button">2000's</button>
-                <div class="accordion-content">
+                <button class="accordion-button">2000's ▼</button>
+                <div class="accordion-content space-x-5">
                     <a href="#">2000</a>
                     <a href="#">2001</a>
                     <a href="#">2002</a>
@@ -49,8 +49,8 @@
             </div>
             <!-- 2010년대 -->
             <div class="accordion-item">
-                <button class="accordion-button">2010's</button>
-                <div class="accordion-content">
+                <button class="accordion-button">2010's ▼</button>
+                <div class="accordion-content space-x-5">
                     <a href="#">2010</a>
                     <a href="#">2011</a>
                     <a href="#">2012</a>
@@ -65,13 +65,37 @@
             </div>
             <!-- 2020년대 -->
             <div class="accordion-item">
-                <button class="accordion-button">2020's</button>
-                <div class="accordion-content">
+                <button class="accordion-button">2020's ▼</button>
+                <div class="accordion-content space-x-5">
                     <a href="#">2020</a>
                     <a href="#">2021</a>
                     <a href="#">2022</a>
                     <a href="#">2023</a>
                 </div>
+            </div>
+        </div>
+    </ul>
+    <ul>· 팀별
+        <div class="accordion-item">
+            <div class="accordion-button">
+                <button>롯데 자이언츠</button>
+                <button>LG 트윈스</button>
+                <button>KIA 타이거즈</button>
+                <button>키움 히어로즈</button>
+                <button>삼성 라이온즈</button>
+                <button>NC 다이노스</button>
+                <button>KT 위즈</button>
+                <button>한화 이글스</button>
+                <button>두산 베어스</button>
+                <button>SSG 렌더스</button>
+            </div>
+        </div>
+    </ul>
+    <ul>· 명예의전당
+        <div class="accordion-item">
+            <div class="accordion-button">
+                <div>투수부문</div>
+                <div>타격부문</div>
             </div>
         </div>
     </ul>
